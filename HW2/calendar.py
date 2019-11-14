e0d57a29cd129946735ad324a6fd5c0810beb2df
Yebin Kim <mareng825@naver.com>
2019-11-15 00:24:34 +0900

    Second Commit

--- HW2/calendar.py
+++ HW2/calendar.py
@@ -1,13 +1,9 @@
 from tkinter import *
+from korean_lunar_calendar import KoreanLunarCalendar
 import calendar
 
-def makeCalendar(yearInput, monthInput):
-    print(yearInput, monthInput)
-    global c
-    c = calendar.month(yearInput, monthInput)
-    dateIndex = c.find('Su')
-    c = c[dateIndex + 2:]
-    return c
+window = Tk()
+weekList = ('월', '화', '수', '목', '금', '토', '일')
 
 def leftBtn():
     global yearInput
@@ -20,7 +16,6 @@ def leftBtn():
     makeCalendar(yearInput, monthInput)
     
 def rightBtn():
-    print("RIGHT")
     global yearInput
     global monthInput
     if monthInput < 12:
@@ -30,38 +25,58 @@ def rightBtn():
         monthInput = 1
     makeCalendar(yearInput, monthInput)
 
-yearInput = int(input("년도를 입력하세요: "))
-monthInput = int(input("월을 입력하세요: "))
-
-c = makeCalendar(yearInput, monthInput)
-print("START", c, "END")
-
-weekList = ['월', '화', '수', '목', '금', '토', '일']
-
-window = Tk()
-
-title = Label(window, text="{}년 {}월".format(yearInput, monthInput))
-week = Label(window, text=weekList)
-dateFrame = Frame(window, width=1600, height=500)
-
-index = 0
-for i in range(5):
-    for j in range(7):
-        day = c[index:index+3]
-        name = Button(dateFrame, text = day)
-        name.grid(row=i, column=j)
-        if index + 3 <= len(c): index += 3
-        
-
 leftBtn = Button(window, text="<", command=leftBtn)
 rightBtn = Button(window, text=">", command=rightBtn)
 
 leftBtn.pack(side=LEFT)
+rightBtn.pack(side=RIGHT)            
+    
+def makeCalendar(yearInput, monthInput):
+    global dateFrame
+    global c
+    for child in dateFrame.winfo_children():
+        child.destroy()
+
+    print(yearInput, monthInput)
+
+    title["text"] = "{}년 {}월".format(yearInput, monthInput)
+    c = calendar.month(yearInput, monthInput)
+    lunar_c = KoreanLunarCalendar()
+   # lunar.setSolarDate(2017, 6, 24)
+    #print(lunar.LunarIsoFormat())
+    dateIndex = c.find('Su')
+    c = c[dateIndex + 2:]
+    index = 0
+    for i in range(6):
+        for j in range(7):
+            day = c[index:index+3]
+            day = day.replace('\n', '')
+            if day.strip() != '':               #day가 비어있지 않으면 음력 추가
+                lunar_day = day.strip()
+                lunar_day = int(lunar_day)
+                lunar_c.setSolarDate(yearInput, monthInput, lunar_day)
+                lunar_day = lunar_c.LunarIsoFormat()
+                lunar_index = lunar_day.find('-')
+                lunar_day = lunar_day[lunar_index+1:]
+                lunar_day = lunar_day.replace('-', '.')
+                day = day + '\n' + lunar_day
+            name = Button(dateFrame, text = day)
+            name.grid(row=i, column=j)
+            if index + 3 <= len(c): index += 3
+        if index >= len(c): break
+    return c
+
+yearInput = int(input("년도를 입력하세요: "))
+monthInput = int(input("월을 입력하세요: "))
+
+title = Label(window, text="{}년 {}월".format(yearInput, monthInput))
+week = Label(window, text=weekList)
+dateFrame = Frame(window, width=3000, height=1000)
+
+c = makeCalendar(yearInput, monthInput)
+
 title.pack()
-rightBtn.pack(side=RIGHT)
 week.pack()
 dateFrame.pack()
-
-
 
 window.mainloop()
